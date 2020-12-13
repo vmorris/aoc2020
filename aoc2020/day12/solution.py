5a59eda702bd892123f5bfdc8c9e5c3cc278ded2
Vance Morris <vmorris@us.ibm.com>
2020-12-13 11:08:24 -0600

    day 12 part 1 complete

--- aoc2020/day12/solution.py
+++ aoc2020/day12/solution.py
@@ -1,8 +1,49 @@
 from aoc2020.util import get_input
 
 
+compass = {0: "N", 90: "E", 180: "S", 270: "W"}
+
+
+class Ship:
+    def __init__(self):
+        self.degree = 90
+        self.direction = "E"
+        self.x = 0
+        self.y = 0
+
+    def forward(self, distance):
+        self.translate(self.direction, distance)
+
+    def translate(self, direction, distance):
+        if direction == "N":
+            self.y += distance
+        elif direction == "E":
+            self.x += distance
+        elif direction == "S":
+            self.y -= distance
+        elif direction == "W":
+            self.x -= distance
+
+    def rotate(self, degrees):
+        self.degree += degrees
+        self.degree %= 360
+        self.direction = compass[self.degree]
+
+
 def solve_part1(entries):
-    return
+    ship = Ship()
+    for instruction in entries:
+        operation = instruction[0]
+        operand = int(instruction[1:])
+        if operation == "F":
+            ship.forward(operand)
+        elif operation == "R":
+            ship.rotate(operand)
+        elif operation == "L":
+            ship.rotate(-operand)
+        else:
+            ship.translate(operation, operand)
+    return abs(ship.x) + abs(ship.y)
 
 
 def solve_part2(entries):
@@ -10,6 +51,6 @@ def solve_part2(entries):
 
 
 if __name__ == "__main__":  # pragma: no cover
-    entries = get_input("aoc2020/CHANGEME/input")
+    entries = get_input("aoc2020/day12/input")
     print(solve_part1(entries))
     print(solve_part2(entries))
--- tests/test_day12.py
+++ tests/test_day12.py
@@ -1,12 +1,12 @@
-from aoc2020.dayCHANGEME import solution
-from aoc2020.util import get_input_as_int
+from aoc2020.day12 import solution
+from aoc2020.util import get_input
 
 
-input_data = get_input_as_int("tests/testinput.dayCHANGEME")
+input_data = get_input("tests/testinput.day12")
 
 
 def test_solve_part1():
-    expected = None
+    expected = 25
     actual = solution.solve_part1(input_data)
     assert expected == actual
 

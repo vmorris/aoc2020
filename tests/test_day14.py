78d2b6e7547caf44a3c8bd0eacb72646696e6577
Vance Morris <vmorris@us.ibm.com>
2020-12-13 16:20:10 -0600

    setup for day 14

---
+++ tests/test_day14.py
@@ -0,0 +1,17 @@
+from aoc2020.day14 import solution
+from aoc2020.util import get_input_as_int
+
+
+input_data = get_input_as_int("tests/testinput.day14")
+
+
+def test_solve_part1():
+    expected = None
+    actual = solution.solve_part1(input_data)
+    assert expected == actual
+
+
+def test_solve_part2():
+    expected = None
+    actual = solution.solve_part2(input_data)
+    assert expected == actual

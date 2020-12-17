33676d650c22d80c7d4d9f722a96c74aae5877ac
Vance Morris <vmorris@us.ibm.com>
2020-12-17 07:38:52 -0600

    day 14 part 1 complete, part 2 partial

--- aoc2020/day14/solution.py
+++ aoc2020/day14/solution.py
@@ -1,11 +1,77 @@
+from pprint import pprint
+
 from aoc2020.util import get_input
 
 
+def int2binstr(value):
+    return f"{int(value):036b}"
+
+
+def apply_mask_part1(value, mask):
+    value = int2binstr(value)
+    for i, _ in enumerate(value):
+        if mask[i] != value[i] and mask[i] != "X":
+            value = value[:i] + mask[i] + value[i + 1 :]
+    return int(value, 2)
+
+
 def solve_part1(entries):
-    return
+    addresses = dict()
+    for entry in entries:
+        operation, operand = (e.strip() for e in entry.split("="))
+        if operation == "mask":
+            mask = operand
+        elif operation.startswith("mem"):
+            address = int(operation.split("[")[1].split("]")[0])
+            value = operand
+            addresses[address] = apply_mask_part1(value, mask)
+    return sum(addresses.values())
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
+
+def set_str_char(s, index, char):
+    return s[:index] + char + s[index + 1 :]
+
+
+def apply_mask_part2(address, mask):
+    addresses = list()
+    num_X = mask.count("X")
+    address = int2binstr(address)
+    for i in range(2 ** num_X):
+        addresses.append(address)
+    indexes = [(index, char) for index, char in enumerate(mask) if char == "X"]
+    indexes = list(reversed(indexes))
+    pprint(indexes)
+    step = 0
+    while step < num_X:
+        groups = list(chunks(addresses, 2 ** step))
+        zero = 0
+        for i, group in enumerate(groups):
+            print(
+                f"{zero}; i:{i}; indexes[step][0]:{indexes[step][0]}; len(group):{len(group)}"
+            )
+        step += 1
+
+    for i, _ in enumerate(address):
+        if mask[i] == "1":
+            address = set_str_char(address, i, "1")
 
 
 def solve_part2(entries):
+    addresses = dict()
+    for entry in entries:
+        operation, operand = (e.strip() for e in entry.split("="))
+        if operation == "mask":
+            mask = operand
+        elif operation.startswith("mem"):
+            address = operation.split("[")[1].split("]")[0]
+            value = operand
+            to_write = apply_mask_part2(address, mask)
     return
 
 
--- tests/test_day14.py
+++ tests/test_day14.py
@@ -1,17 +1,33 @@
 from aoc2020.day14 import solution
-from aoc2020.util import get_input_as_int
+from aoc2020.util import get_input
 
 
-input_data = get_input_as_int("tests/testinput.day14")
+input_data = get_input("tests/testinput.day14")
+input_data_2 = get_input("tests/testinput.day14.2")
+
+
+def test_int2binstr():
+    value = 64
+    expected = "000000000000000000000000000001000000"
+    actual = solution.int2binstr(value)
+    assert expected == actual
+
+
+def test_apply_mask_part1():
+    mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+    value = 11
+    expected = 73
+    actual = solution.apply_mask_part1(value, mask)
+    assert expected == actual
 
 
 def test_solve_part1():
-    expected = None
+    expected = 165
     actual = solution.solve_part1(input_data)
     assert expected == actual
 
 
-def test_solve_part2():
-    expected = None
-    actual = solution.solve_part2(input_data)
-    assert expected == actual
+# def test_solve_part2():
+#    expected = 208
+#    actual = solution.solve_part2(input_data_2)
+#    assert expected == actual

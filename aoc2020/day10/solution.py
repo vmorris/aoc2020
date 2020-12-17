bab453d062de4b0a85aa9e5e7a61dbd55b721996
Vance Morris <vmorris@us.ibm.com>
2020-12-17 07:49:30 -0600

    use proper get input function on day 10

--- aoc2020/day10/solution.py
+++ aoc2020/day10/solution.py
@@ -64,6 +64,6 @@ def solve_part2(entries):
 
 
 if __name__ == "__main__":  # pragma: no cover
-    entries = get_input("aoc2020/day10/input")
+    entries = get_input_as_int("aoc2020/day10/input")
     print(solve_part1(entries))
     print(solve_part2(entries))
